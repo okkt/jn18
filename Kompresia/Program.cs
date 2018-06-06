a79929ea275c76b3151e3df4e633529e9b31a6e3
Rezanina Lukas <rez0041@vsb.cz>
2018-06-06 19:11:09 +0200

    upraveny huffman

--- Kompresia/Program.cs
+++ Kompresia/Program.cs
@@ -11,25 +11,28 @@ namespace Kompresia
     {
         static void Main(string[] args)
         {
-            string path = "C:/Users/rez0041/source/repos/Kompresia/Kompresia/bin/Debug/part.xml";
-             string path2x = "C:/Users/rez0041/source/repos/Kompresia/Kompresia/File/part.xml.lz";
-            string path2 = "C:/Users/rez0041/source/repos/Kompresia/Kompresia/File/part.xml";
-            string path3 = "C:/Users/rez0041/source/repos/Kompresia/Kompresia/part.xml";
-
-            //var b = new File.CompressedFileReader(path: path);
-            //var bb = new File.CompressedFileWriter();
-
-            //var a = new Huffman.AdaptiveHuffman();
-            LZ.LZ77 c = new LZ.LZ77();
-            //var d = new JSON.JsonH();
-            c.Compress(path: path2);
+            
            
-            c.Decompress(path: path2x);
-            // a.Decompress(path: path3);
-            //bb.CompressedFileWriter();
+            string pathH = "C:/Users/rez0041/source/repos/Kompresia/Kompresia/Huffman/part.xml";
+            string pathL = "C:/Users/rez0041/source/repos/Kompresia/Kompresia/LZ/part.xml";
+            string pathL2 = "C:/Users/rez0041/source/repos/Kompresia/Kompresia/LZ/part.xml.lz";
 
-            string valueString = "Your string";
-            Console.Write(valueString);
+
+
+
+            Huffman.AdaptiveHuffman huff = new Huffman.AdaptiveHuffman();
+            LZ.LZ77 algLZ = new LZ.LZ77();
+
+            
+            huff.Compress(path: pathH);
+            //huff.Decompress(path: pathH);
+            //algLZ.Compress(path: pathL);
+            //algLZ.Decompress(path: pathL2);
+
+
+
+            string valueString = "Succed";
+            Console.WriteLine(valueString);
 
         }
     }
